2c168b09a08f7ad6021b0b603076b03300d9cb99
ksaipavanr <ksaipavanr@gmail.com>
2025-06-05 16:43:57 +0530

    updated the badges

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -5,22 +5,6 @@ import './Gallery.scss';
 function Gallery() {
     const images = [
         {
-            imageUrl: 'https://images.credly.com/size/680x680/images/73e4a58b-a8ef-41a3-a7db-9183dd269882/image.png',
-            link: 'https://www.credly.com/earner/earned/badge/d357d137-1f4a-492b-92ac-e0b4e27e3414'
-        },
-        {
-            imageUrl: 'https://images.credly.com/size/680x680/images/2f7b0627-48a0-4894-8d46-3245bdfe0463/image.png',
-            link: 'https://www.credly.com/earner/earned/badge/23c3bf33-bd42-4b30-bd16-56ed6f0dccb7'
-        },
-        {
-            imageUrl: 'https://images.credly.com/size/680x680/images/af8c6b4e-fc31-47c4-8dcb-eb7a2065dc5b/I2CS__1_.png',
-            link: 'https://www.credly.com/earner/earned/badge/6dcd602b-d2de-4097-9734-e267e6c30cff'
-        },
-        {
-            imageUrl: 'https://images.credly.com/size/680x680/images/ec621e2a-c8f0-4459-806c-ae11829d372a/image.png',
-            link: 'https://www.credly.com/badges/477d0a3b-f8f6-4f02-9306-e3e523f29ad5'
-        },
-        {
             imageUrl: 'https://images.credly.com/size/680x680/images/00634f82-b07f-4bbd-a6bb-53de397fc3a6/image.png',
             link: 'https://www.credly.com/earner/earned/badge/477d0a3b-f8f6-4f02-9306-e3e523f29ad5'
         },
